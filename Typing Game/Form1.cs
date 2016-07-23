cab8d922f8db99ef4e8428249bfa0349156fe2e8
go4zee <go4zeee@gmail.com>
2016-07-23 14:04:38 -0700

    Add project files.

---
+++ Typing Game/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typing_Game
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if(listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game Over");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgresBar.Value = 800 - timer1.Interval;
+                stats.Update(true);
+            }
+            else
+            {
+                stats.Update(false);
+            }
+
+            correctLabel.Text = "Correct: " + stats.Correct;
+            missedLabel.Text = "Missed: " + stats.Missed;
+            totalLabel.Text = "Total: " + stats.Total;
+            accuracyLabel.Text = "Accuracy: " + stats.Accuracy + "%";
+        }
+    }
+
+    class Stats
+    {
+        public int Total = 0;
+        public int Missed = 0;
+        public int Correct = 0;
+        public int Accuracy = 0;
+
+        public void Update(bool correctKey)
+        {
+            Total++;
+
+            if(!correctKey)
+            {
+                Missed++;
+            }
+            else
+            {
+                Correct++;
+            }
+
+            Accuracy = 100 * Correct / (Missed + Correct);
+        }
+    }
+}
